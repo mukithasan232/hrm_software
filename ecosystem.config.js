4e2a8e61dd0bb43ace3f9f594eaaf781c4673b0b
MD MUKIT HASAN <144932194+mukithasan232@users.noreply.github.com>
2026-07-11 17:42:03 +0600

    fix(pm2): Use Next.js production binary for next-web

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'next-web',
-      script: 'node server.cjs',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
